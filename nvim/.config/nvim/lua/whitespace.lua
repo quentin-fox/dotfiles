cd4d239a561041619eee857054f8c8f5baabb163
Quentin Fox <quentinbennettfox@gmail.com>
2022-04-27 01:31:29 -0400

    make list only a window-scoped option

--- nvim/.config/nvim/lua/whitespace.lua
+++ nvim/.config/nvim/lua/whitespace.lua
@@ -5,7 +5,7 @@ function M.use_tabs(width)
   vim.bo.tabstop = width
   vim.bo.softtabstop = width
   vim.bo.shiftwidth = width
-  vim.bo.list = false
+  vim.wo.list = false
 end
 
 function M.use_spaces(width)
@@ -13,7 +13,7 @@ function M.use_spaces(width)
   vim.bo.tabstop = width
   vim.bo.softtabstop = width
   vim.bo.shiftwidth = width
-  vim.bo.list = true
+  vim.wo.list = true
 end
 
 return M
