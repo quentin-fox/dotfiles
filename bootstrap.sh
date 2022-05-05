0be1a3ff6d4de027c0b5f79bcbecd72d7167f2b3
Quentin Fox <quentinbennettfox@gmail.com>
2022-05-05 15:59:46 -0400

    manage java with asdf, use zulu 17

--- bootstrap.sh
+++ bootstrap.sh
@@ -80,6 +80,7 @@ asdf plugin add python https://github.com/danhper/asdf-python.git
 asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin add lua https://github.com/Stratus3D/asdf-lua.git
 asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
 
 info 'Installing global versions of asdf plugins'
 asdf install
